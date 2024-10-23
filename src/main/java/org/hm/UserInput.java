60af62ba6817b1d0132ede5b45ce434198ade46a
Hamza <hamza46awais@gmail.com>
2024-10-23 15:19:35 +0100

    Edit: completed task 3

--- src/main/java/org/hm/UserInput.java
+++ src/main/java/org/hm/UserInput.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Input name: ");
         String name = scanner.nextLine();
         System.out.printf("Hello %s!\n", name);
+
+        System.out.println("Please input number1: ");
+        int num1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Please input number2: ");
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        int num3 = num1 + num2;
+        System.out.printf("Sum is: %s", num3);
+
     }
 }
