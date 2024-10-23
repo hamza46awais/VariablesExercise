51b7d48cd9a5a49b16993c534652bd9d94d65d49
Matt Nudds <matthewnudds1@gmail.com>
2024-10-23 15:06:50 +0100

    EDIT: up to 2.5 but the division doesnt work as they're ints

--- src/main/java/org/hm/Numbers.java
+++ src/main/java/org/hm/Numbers.java
@@ -2,6 +2,13 @@ package org.hm;
 
 public class Numbers {
     public static void main(String[] args) {
+        int num1 = 1, num2 = 2;
+
+        System.out.println(num1+num2);
+
+        int result = num1 / num2;
+
+        System.out.println(result);
 
     }
 }
