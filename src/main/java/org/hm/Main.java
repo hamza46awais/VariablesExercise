fafc729b22400cf88e06ad725f6f12981d21687f
Hamza <hamza46awais@gmail.com>
2024-10-23 14:58:28 +0100

    EDIT: completed task 1

--- src/main/java/org/hm/Main.java
+++ src/main/java/org/hm/Main.java
@@ -4,5 +4,9 @@ package org.hm;
 public class Main {
     public static void main(String[] args) {
 
+        String firstName = "Alan", lastName = "Barry";
+        String name = firstName + " " + lastName;
+
+        System.out.printf("Name: %s", name);
     }
 }
