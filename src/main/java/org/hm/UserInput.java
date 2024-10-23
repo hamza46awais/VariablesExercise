6e0930a64153e58e9dafed03c8f3d2947386aecb
Hamza <hamza46awais@gmail.com>
2024-10-23 15:16:32 +0100

    Edit: completed up until 3.2

--- src/main/java/org/hm/UserInput.java
+++ src/main/java/org/hm/UserInput.java
@@ -1,7 +1,12 @@
 package org.hm;
 
+import java.util.Scanner;
+
 public class UserInput {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello %s!\n", name);
     }
 }
