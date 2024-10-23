a58d5683aff85f07a572e0dc4728f059a4aa6a56
Matt Nudds <matthewnudds1@gmail.com>
2024-10-23 16:28:30 +0100

    EDIT: hangman almost done, problem with txt file

--- src/main/java/org/hm/Hangman.java
+++ src/main/java/org/hm/Hangman.java
@@ -1,34 +1,55 @@
 package org.hm;
 
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Hangman {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String answer = "MANNER";
-        String usersAnswer = "------";
-        boolean gameOngoing = true;
+        Random rn = new Random();
+        int randN = rn.nextInt(4000) + 1;
 
-        while (gameOngoing) {
+        //String answer = "";
+        try {
+            String answer = Files.readAllLines(Paths.get("4000-most-common-english-words-csv.txt")).get(randN);
 
-            System.out.println("Guess: ");
-            String guessStr = (scanner.nextLine()).toUpperCase();
-            char guessChar = (guessStr.charAt(0));
-
-            for (int i = 0; i < answer.length(); i++) {
-                char c = answer.charAt(i);
-
-                if (guessChar == c) {
-                    usersAnswer = usersAnswer.substring(0, i) + guessChar + usersAnswer.substring(i + 1);
-                    System.out.println(usersAnswer);
-                }
-
-                if (usersAnswer.equals(answer)) gameOngoing = false;
+            StringBuilder dashes = new StringBuilder();
+            for (int i = 0; i < answer.length(); i++){
+                dashes.append("-");
             }
 
+            String dashesStr = dashes.toString();
+
+            boolean gameOngoing = true;
+
+            while (gameOngoing) {
+                System.out.println(dashesStr.length());
+                System.out.println("Guess: ");
+                String guessStr = (scanner.nextLine()).toUpperCase();
+                char guessChar = (guessStr.charAt(0));
+
+                for (int i = 0; i < answer.length(); i++) {
+                    char c = answer.charAt(i);
+
+                    if (guessChar == c) {
+                        dashesStr = dashesStr.substring(0, i) + guessChar + dashesStr.substring(i + 1);
+                        System.out.println(dashesStr);
+                    }
+
+                    if (dashesStr.equals(answer)) gameOngoing = false;
+                }
+
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
         }
 
     }
