41fd9383f48f6622a1b9d05f9eda3d9b16b1157b
Hamza <hamza46awais@gmail.com>
2024-10-23 16:03:58 +0100

    EDIT: fix ability to check if user's guess is correct

--- src/main/java/org/hm/Hangman.java
+++ src/main/java/org/hm/Hangman.java
@@ -8,18 +8,19 @@ public class Hangman {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String answer = "Manner";
+        String answer = "MANNER";
         String usersAnswer = "------";
 
         System.out.println("Guess: ");
-        String guessStr = scanner.nextLine();
-        char guessChar = (guessStr.charAt(0);
+        String guessStr = (scanner.nextLine()).toUpperCase();
+        char guessChar = (guessStr.charAt(0));
 
         for (int i = 0; i < answer.length(); i++) {
             char c = answer.charAt(i);
 
             if (guessChar == c){
-                usersAnswer.charAt(i) = guessChar;
+                usersAnswer = usersAnswer.substring(0,i)+guessChar+usersAnswer.substring(i+1);
+                System.out.println(usersAnswer);
             }
         }
 
